c5a85b141088c94d49dd2138aab4d5fbe58d236d
michael15346 <mzadorskyy@gmail.com>
2026-08-21 20:11:54 +0300

    chore: collab bullshit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import jax.core
+import jax.interpreters.xla as xla_inter
+import jax._src.xla_bridge as xb
+
+# shim for JAX 0.7 removal
+if not hasattr(xla_inter, 'pytype_aval_mappings'):
+    xla_inter.pytype_aval_mappings = jax.core.pytype_aval_mappings
+
+# tfp also does: jax.interpreters.xla.Device etc.
+if not hasattr(xla_inter, 'Device'):
+    xla_inter.Device = xb.Device
 import json
 import importlib
 import os
